8c879b47ed4aed535a97364a5ec66765ce5f923d
Aku Sarma <akusarma1702@gmail.com>
2024-10-16 19:53:47 +0530

    completed navbar and footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className='bg-slate-800 p-8 w-full text-white flex justify-center items-center'>
         &copy; Aku Sarma 2024
     </footer>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,27 +1,98 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import logo from '../images/logo.png'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggles the menu visibility
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/browse-cars">browse cars</Link>
-        </li>
-        <li>
-          <Link to="/car-details">Car detail</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/registration">Login / Logout</Link>
-        </li>
-      </ul>
+    <nav className="border-gray-200 bg-slate-800">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link
+          to="/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <img
+            src={logo}
+            className="h-8"
+            alt="rent-a-car logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+            rent-a-car
+          </span>
+        </Link>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <Link to="/registration">Register</Link>
+          </button>
+          <button
+            onClick={handleToggleMenu}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-cta"
+            aria-expanded={isMenuOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`items-center justify-between ${
+            isMenuOpen ? "block" : "hidden"
+          } w-full md:flex md:w-auto md:order-1`}
+          id="navbar-cta"
+        >
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+            <li>
+              <Link
+                to="/"
+                className="block py-2 px-3 md:p-0 text-white rounded md:bg-transparent hover:underline  "
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/browse-cars"
+                className="block py-2 px-3 md:p-0 text-white rounded md:bg-transparent hover:underline "
+              >
+                Browse Cars
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contact"
+                className="block py-2 px-3 md:p-0 text-white rounded md:bg-transparent hover:underline "
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;

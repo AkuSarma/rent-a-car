591226508c2bd6f5a38be1ec9b2c0e51c83f53ca
Aku Sarma <akusarma1702@gmail.com>
2024-10-16 19:01:09 +0530

    setup react router and tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import CarDetails from './pages/CarDetails';
+import BrowseCars from './pages/BrowseCars';
+import Registration from './pages/Registration';
+import Contact from './pages/Contact';
+import NoPage from './pages/NoPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home/>} />
+            <Route path="browse-cars" element={<BrowseCars />} />
+            <Route path="car-details" element={<CarDetails />} />
+            <Route path="registration" element={<Registration />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
